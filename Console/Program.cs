b751a7f102959a5516d65b982ebbde164fca0649
TauTauwk <s1168742@student.windesheim.nl>
2022-11-21 13:25:10 +0100

    wachtwoord check en email check

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using DataModel;
+
+RegisterCheck registerCheck = new RegisterCheck();
+bool resultaat;
+
+Console.WriteLine("Voer Email in: ");
+string email = Console.ReadLine();
+resultaat = registerCheck.CheckEmail(email);
+Console.WriteLine("Je Email is" + resultaat);
+
+
+Console.WriteLine("Voer wachtwoord in:");
+string wachtwoord = Console.ReadLine();
+resultaat = registerCheck.CheckPassword(wachtwoord);
+Console.WriteLine("Je wachtwoord is " + resultaat);
---
+++ DataModel/RegisterCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace DataModel {
+    public class RegisterCheck {
+        
+        public bool CheckPassword(string password) {
+            if (PasswordLength(password) && PasswordContainsNumber(password) && PasswordContainsCapitalLetter(password)) {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckEmail(string email) {
+            if (email.EndsWith("student.windesheim.nl") && email.StartsWith("s")) {
+                return true;
+            }
+            return false;
+        }
+
+        private bool PasswordLength(string password) {
+            return password.Length >= 8;
+        }
+
+        private bool PasswordContainsNumber(string password) {
+            return password.Any(char.IsDigit);
+        }
+
+        private bool PasswordContainsCapitalLetter(string password) {
+            return password.Any(char.IsUpper);
+        }
+
+
+    }
+}
