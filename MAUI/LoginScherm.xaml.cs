d016de73bdf66db265f224ee90b43d77a1a23c64
migaokken <104058503+migaokken@users.noreply.github.com>
2022-11-23 14:08:54 +0100

    f

---
+++ MAUI/LoginScherm.xaml.cs
@@ -0,0 +1,23 @@
+namespace MAUI;
+
+public partial class InlogScherm : ContentPage
+{
+    Button b = new Button();
+    Button b2 = new Button();
+   
+    public InlogScherm()
+    {
+        InitializeComponent();
+        b.Clicked += Inlog;
+        b2.Clicked += WachtwoordVergeten;
+    }
+    private void Inlog(object sender, EventArgs e)
+    {
+
+    }
+    private void WachtwoordVergeten(object sender, EventArgs e)
+    {
+        
+    }
+   
+}
