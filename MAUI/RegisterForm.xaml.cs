e4090f9c8e79eeb0cc5b11dda6ea3bef52e0ce12
alireza770 <s1173231@student.windesheim.nl>
2022-11-28 12:47:14 +0100

    Registerform display completed

--- MAUI/RegisterForm.xaml.cs
+++ MAUI/RegisterForm.xaml.cs
@@ -1,3 +1,7 @@
+
+
+
+
 namespace MAUI;
 
 public partial class RegisterForm : ContentPage
@@ -5,17 +9,83 @@ public partial class RegisterForm : ContentPage
 	private string email;
     private string voornaam;
     private string achternaam;
-
+    private DateTime geboortedatum;
+    private string geslacht;
     private string tussenvoegsel;
     private string wachtwoord;
     private string voorkeur;
     private string opleiding;
-    
+    private string locatie;
+    private string interesses;
+
+
+
+
+
+
     public RegisterForm() 
     {
         InitializeComponent();
         
     }
+
+    public void SaveEvent (object sender, EventArgs e)
+    {
+        //Declaring objects
+        voorkeur = Voorkeur.ToString();
+        opleiding = Opleiding.ToString();
+        locatie = Locatie.ToString();
+        interesses = Interesses.ToString();
+
+    }
+
+
+    public void RegisterBtnEvent(object sender, EventArgs e)
+    {
+
+        //declaring objects by form values
+        email = Email.Text;
+        voornaam = Voornaam.Text;
+        tussenvoegsel = Tussenvoegsel.Text;
+        achternaam = Achternaam.Text;
+        wachtwoord = Wachtwoord.Text;
+        geboortedatum = new DateTime(Geboortedatum.Date.Year, Geboortedatum.Date.Month, Geboortedatum.Date.Day);
+        geslacht = Geslacht.SelectedItem.ToString();
+
+        
+
+        //setting objects visible to proceed the registerform
+        LblVoorkeur.IsVisible = true;
+        Voorkeur.IsVisible = true;
+        LblOpleiding.IsVisible = true;
+        Opleiding.IsVisible = true;
+        LblLocatie.IsVisible = true;
+        Locatie.IsVisible = true;
+        LblInteresses.IsVisible = true;
+        Interesses.IsVisible = true;
+
+
+        // Making objects unvisible to proceed the registerform
+        Email.IsVisible = false;
+        Lblemail.IsVisible = false;
+        Voornaam.IsVisible = false;
+        LblVoornaam.IsVisible = false;
+        Tussenvoegsel.IsVisible = false;
+        LblTussenvoegsel.IsVisible = false;
+        Achternaam.IsVisible = false;
+        LblAchternaam.IsVisible = false;
+        Wachtwoord.IsVisible = false;
+        LblWachtwoord.IsVisible = false;
+        Geboortedatum.IsVisible = false;
+        LblGeboortedatum.IsVisible = false;
+        Registreer.IsVisible = false;
+        LblGeslacht.IsVisible = false;
+        Geslacht.IsVisible=false;
+        //Opslaan button visible
+        Opslaan.IsVisible = true;
+    }
+
+
 
 
 }
