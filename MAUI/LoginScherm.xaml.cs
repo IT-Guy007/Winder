462693ad2fc7de17e920fe8c1158b023b5d65d5c
TauTauwk <s1168742@student.windesheim.nl>
2022-11-28 15:32:58 +0100

    volledig

--- MAUI/LoginScherm.xaml.cs
+++ MAUI/LoginScherm.xaml.cs
@@ -1,13 +1,17 @@
 
 
+using DataModel;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MAUI;
-public partial class InlogScherm : ContentPage
+public partial class LoginScherm : ContentPage
 {
     Button b = new Button();
     Button b2 = new Button();
-   
-    public InlogScherm()
+    Database database = new Database();
+
+
+    public LoginScherm()
     {
         InitializeComponent();
         b.Clicked += Inlog;
@@ -16,6 +20,14 @@ public partial class InlogScherm : ContentPage
     }
     private void Inlog(object sender, EventArgs e)
     {
+        var Email = Emailadres.Text;
+        var Password = Wachtwoord.Text;
+        if (database.checkLogin(Email, Password)) {
+            Navigation.PushAsync(new MainPage());
+        }
+        else {
+            FoutmeldingInloggen.IsVisible = true;
+        }
 
     }
     private void WachtwoordVergeten(object sender, EventArgs e)
