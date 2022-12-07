b40c7f2ac14efbd3b99a6190c1596041f509c701
migaokken <104058503+migaokken@users.noreply.github.com>
2022-12-07 17:18:30 +0100

    d

--- Winder/MatchPage.xaml.cs
+++ Winder/MatchPage.xaml.cs
@@ -23,7 +23,7 @@ public partial class MatchPage : ContentPage {
         verticalStackLayout.Spacing = 10;
         //Images
         if (_profileQueue.Count == 0) {
-            if(Authentication._currentUser.profilePicture.Height != 0 ) {
+            if(Authentication._currentUser.firstName == null ) {
 
                 var profileImage = new Image
                 {
