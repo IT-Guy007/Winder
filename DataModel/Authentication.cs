291404631cab90e9abec39e21376f9d3f4d42613
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2022-12-09 14:04:46 +0100

    Upgrade to .7

--- DataModel/Authentication.cs
+++ DataModel/Authentication.cs
@@ -7,14 +7,13 @@ using System.Text;
 
 public class Authentication {
     
-    public User _currentUser { get; set; }
+    public static User _currentUser { get; set; }
     private LoggedState _loggedState { get; set; }
     private AccountState _accountState { get; set; }
     
     public Authentication() {
         _loggedState = LoggedState.signedOut;
         _accountState = AccountState.inactive;
-        _currentUser = new User();
     }
 
     //Defining state
@@ -124,11 +123,9 @@ public class Authentication {
 
 
     //verstuurd de mail
-    public void SendEmail(string email, string body, string subject)
-    {
+    public void SendEmail(string email, string body, string subject) {
         //zet de client op
-        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
-        {
+        SmtpClient smtpClient = new SmtpClient("smtp.gmail.com") {
             Port = 587,
             UseDefaultCredentials = false,
             Credentials = new NetworkCredential("thewinderapp@gmail.com", "xltbqbsyderpqsxp"),
@@ -137,8 +134,7 @@ public class Authentication {
         };
 
         // maakt de mail aan
-        MailMessage mailMessage = new MailMessage
-        {
+        MailMessage mailMessage = new MailMessage {
             From = new MailAddress("thewinderapp@gmail.com"),
             Subject = subject,
             Body = body,    
