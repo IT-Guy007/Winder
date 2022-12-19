50fb547953c00a837cb38c27b85be607381d4ba3
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2022-12-19 12:47:44 +0100

    Added the form and the reference to the function

--- Winder/ChatView.xaml.cs
+++ Winder/ChatView.xaml.cs
@@ -5,13 +5,11 @@ namespace Winder;
 public partial class ChatView : ContentPage {
     
     public string originPage;
-    
-    
+
     //MAUI
     private ScrollView scrollView;
     private StackLayout verticalStackLayout;
-    
-    //Chatmodel
+
 
     public ChatView(User sendFromUser, User sendToUser) {
         
@@ -27,24 +25,60 @@ public partial class ChatView : ContentPage {
 
 
 
-        if(DatabaseChangeListener._chatCollection.Count == 0)
-        foreach (var message in DatabaseChangeListener._chatCollection) {
-            Label chatMessage = new Label();
-            chatMessage.Text = message.message;
-            chatMessage.FontSize = 20;
-            if (message.fromUser == Authentication._currentUser.email) {
-                chatMessage.HorizontalTextAlignment = TextAlignment.End;
-                chatMessage.BackgroundColor = Color.FromArgb("#90EE90");
-            } else {
-                chatMessage.HorizontalTextAlignment = TextAlignment.Start;
-                chatMessage.BackgroundColor = Color.FromArgb("#ADD8E6");
+        if (DatabaseChangeListener._chatCollection.Count == 0) {
+            Label noMessagesFound = new Label {
+                Text = "No messages found", 
+                HorizontalOptions = LayoutOptions.Center, 
+                VerticalOptions = LayoutOptions.Center,
+                FontSize = 20
+            };
+            verticalStackLayout.Add(noMessagesFound);
+        } else  {
+
+            foreach (var message in DatabaseChangeListener._chatCollection) {
+                Label chatMessage = new Label();
+                chatMessage.Text = message.message;
+                chatMessage.FontSize = 20;
+                
+                if (message.fromUser == Authentication._currentUser.email) {
+                    chatMessage.HorizontalTextAlignment = TextAlignment.End;
+                    chatMessage.BackgroundColor = Color.FromArgb("#90EE90");
+                } else {
+                    chatMessage.HorizontalTextAlignment = TextAlignment.Start;
+                    chatMessage.BackgroundColor = Color.FromArgb("#ADD8E6");
+                }
+
+                verticalStackLayout.Add(chatMessage);
             }
-            verticalStackLayout.Add(chatMessage);
         }
         
+        //TextInput
+        StackLayout inputStackLayout = new StackLayout { Orientation = StackOrientation.Horizontal, VerticalOptions = LayoutOptions.End };
+        Entry chatInput = new Entry {
+            Placeholder = "Type your message here",
+            HorizontalOptions = LayoutOptions.FillAndExpand,
+            VerticalOptions = LayoutOptions.End,
+            FontSize = 20
+        };
         
+        Button sendButton = new Button {
+            Text = "Send",
+            HorizontalOptions = LayoutOptions.End,
+            VerticalOptions = LayoutOptions.End,
+            FontSize = 20
+        };
+        sendButton.Clicked += async () => {
+            if (chatInput.Text != "") {
+                await DatabaseChangeListener.SendMessage(chatInput.Text);
+                chatInput.Text = "";
+            }
+        };
         
+        inputStackLayout.Add(chatInput);
+        inputStackLayout.Add(sendButton);
+        verticalStackLayout.Add(inputStackLayout);
         
+
         //Set content
         verticalStackLayout.BackgroundColor = Color.FromArgb("#CC415F");
         scrollView.BackgroundColor = Color.FromArgb("#CC415F");
