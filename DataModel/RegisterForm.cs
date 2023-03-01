c4c155dc646c5baba80a013feab99c63f68cee22
alireza770 <s1173231@student.windesheim.nl>
2023-03-01 17:41:20 +0100

    MVC Registration progress

---
+++ DataModel/RegisterForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataModel
+{
+    public class RegisterForm
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string GenderPreference { get; set; }
+        public byte[] ProfilePicture { get; set; }
+        public string Bio { get; set; }
+        public string Major { get; set; }
+        public bool ProfileActivation { get; set; }
+        public List<string> ChosenInterests { get; set; }
+
+       
+    }
+}
