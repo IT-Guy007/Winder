8eb3f832f0352edc520e75fdabaf0640548f8d6c
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2023-03-02 12:52:33 +0100

    Photos repository Unit tests

--- Unittest/Repositories/PhotosTest.cs
+++ Unittest/Repositories/PhotosTest.cs
@@ -15,8 +15,20 @@ public class PhotosTest
             .Build();
         _photosRepository = new PhotosRepository(configuration);
         
-        
     }
-    
-    
+
+    [TestCase(new byte[0],"s11657077@student.windesheim.nl", ExpectedResult = true)]
+    public bool AddPhotoTest(byte[] image,string email) {
+        return _photosRepository.AddPhoto(image, email);
+    }
+
+    [TestCase("s11657077@student.windesheim.nl", ExpectedResult = true)]
+    public bool DeleteAllPhotosTest(string email) {
+        return _photosRepository.DeleteAllPhotos(email);
+    }
+
+    [TestCase("s11657077@student.windesheim.nl")]
+    public void GetPhotosTest(string email) {
+        Assert.IsNotNull(_photosRepository.GetPhotos(email));
+    }
 }
